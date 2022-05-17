53942e4fe3814657db7ce383460d2bf47c4a9a4c
lgraham20 <95656453+lgraham20@users.noreply.github.com>
2022-05-17 16:57:08 -0500

    Add files via upload

---
+++ 2D_SNPE.py
@@ -0,0 +1,116 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sbi import utils as utils
+from sbi import analysis as analysis
+from sbi.inference import SNPE, prepare_for_sbi, simulate_for_sbi
+
+
+### Ipmorting Global Constants
+### SNPE_vars contains global variables, and SNPE_func
+### contains a few custom functions involved in the 
+### Bayesian analaysis and plotting the results.
+from SNPE_func import *
+from SNPE_vars import *
+
+### If hard_mode is true, the mock observation will 
+### be one that base SNPE w/ MAF struggled to
+### identify as a Gaussian.
+hard_mode = False 
+
+### The filepath determines where the resulting
+### pairplots will be generated and named.
+filepath = 'Random_Obs_Results/6NSFGaussian.png'
+
+### Defining our Prior
+### This default prior is a uniform distribution, with the 
+### same bounds on each parameter. 
+prior = utils.BoxUniform(low=low*torch.ones(num_dim), high=high*torch.ones(num_dim))
+
+### Creating a simulated data observation
+### Normally, this will just draw a set of 
+### random pairs of points from a 2D Gaussian
+### distribution and takes their means. 
+### If Hard Mode, it copies the bad 
+### data set instead.
+if not hard_mode:
+    observation = theta_true + sigma_true*torch.randn((n_obs,num_dim))
+    observed_mean = torch.mean(observation,0)
+    print('Here is the observed data: ' + str(observation) + '\n' + 'Here is the mean: ' + str(observed_mean))
+else:
+    observation = difficult_obs
+    observed_mean = torch.mean(observation,0)
+    print('Here is the observed mean: ' + str(observed_mean))
+
+### This is the simulator that our SBI will use to generate simulated data 
+### and train the neural net. Much like our mock observation, it takes 
+### in a tensor of means, draws 10 random points from a 2D Gaussian 
+### centered on the means, and then averages them together for 
+### a summary statistic. 
+def simulator(theta):
+    simobs = theta + sigma_true*torch.randn((n_obs,num_dim))
+    sim_mean = torch.mean(simobs,0)
+    return sim_mean
+
+
+###########################################
+###########################################
+## Now it's time to begin the SBI. ########
+###########################################
+###########################################
+
+### Checks that our prior and simulator will work with SBI.
+simulator, prior = prepare_for_sbi(simulator, prior)
+
+##########################################################################################
+###################### Density Estimator Info ############################################
+### If you'd like to modify the meta-parameters, change the dense_estimator            ###
+### variable and plug it in for density_estimator in the inference definition.         ###
+### Alternatively, the density_estimator accepts a few prebuilt configurations         ###
+### identified with strings.                                                           ###
+### Use Neural Spline Flow with 'nsf' and Masked Autoregressive Flow with 'maf'.       ###
+### 'mdn' and 'made' are also available. They have not been tested.                    ###
+### If you don't specify a density estimator, the default 'maf' will be chosen.        ###
+###                                                                                    ###
+### The posterior_nn() function may also be imported and used to design                ###
+### a custom probability density estimator.                                            ###
+### from sbi.utils.get_nn_models import posterior_nn                                   ###
+### dense_estimator = posterior_nn(model='nsf', hidden_features=100, num_transforms=10)###
+##########################################################################################
+##########################################################################################
+
+### Defines our inference object, which represents the neural network.
+inference = SNPE(prior=prior,density_estimator='nsf')
+
+### Creates a list to hold each posterior the neural net generates
+### and defines are first proposal for the distribution as the prior.
+posteriors = []
+proposal = prior
+
+### For an explanation on multiround inference, https://www.mackelab.org/sbi/tutorial/03_multiround_inference/
+### In short, this loop trains our neural network across the entire proposed parameter space ("proposal"),
+### then it is fed the observation which it uses to narrow the parameter space and focus in on the area
+### closest to our observation.
+for _ in range(n_runs):
+    #Uses our simulator to generate a number (sim_count) of mock observations across our
+    #proposed parameter space.
+    theta, x = simulate_for_sbi(simulator, proposal, num_simulations= sim_count,num_workers=2) 
+    
+    #Adds the simulated data to the inference object and trains the neural net on it.
+    density_estimator = inference.append_simulations(theta, x, proposal=proposal).train() 
+    
+    #Creates the posterior (posterior = p(theta | x), probability of a parameter given observation)
+    posterior = inference.build_posterior(density_estimator)
+    #Adds that posterior to the list of posteriors
+    posteriors.append(posterior)
+    #Narrows our parameter space to ones the posterior thinks are likely given
+    #our observation.
+    proposal = posterior.set_default_x(observed_mean)
+    #Then, loop repeats with the narrowed parameter space.
+
+### Plotting and saving our results.
+### In the resulting plot, the green static pairplot is the Bayesian approximation,
+### while the blue evolving plot is the SBI approximation. Usually it converges somewhat
+### towards the Bayesian. 
+fig = plot_chains(pdims = dimens_plot,posts = posteriors,obsv = observation,samps = 10000)
+plt.savefig(filepath)  
---
+++ SNPE_func.py
@@ -0,0 +1,109 @@
+from SNPE_vars import *
+import numpy as np
+from chainconsumer import ChainConsumer
+from torch.distributions.multivariate_normal import MultivariateNormal
+import matplotlib.pyplot as plt
+
+#######################################################################
+#Here I'm defining the pair plot maker, creates a chain               #
+#of classic Bayesian analysis vs. SBI analysis given an SBI posterior,#
+#an observation, and a number of samples to draw.                     #
+#######################################################################
+def pairplot_comp(post,obs,ns):
+    ### Resets or Chain Consumer variable (might not be necessary)
+    c =[] 
+    ### Takes the mean of our mock observation
+    obs_mu = torch.mean(obs,0) 
+
+    ### Generates the SBI sample data from the posterior distribution.
+    ### The specific chain consumer function used has problems with 
+    ### torch, so it has to be converted into a numpy array.
+    sbi_data = np.array(post.sample((ns,), x=obs_mu))
+
+    ### Generating the Bayesian pairplot samples
+    ### Covariance matrix based on data
+    cov = torch.cov(obs.T)/np.sqrt(n_obs)
+    ### m defins a Gaussian random generator from Torch
+    ### based on our observed mean + covariance
+    m = MultivariateNormal(obs_mu,cov)
+    bsamps = torch.ones(ns,2)
+    ### Pulling ns random points from the Bayesian guess, 
+    ### converted to Numpy for same reason above.
+    for el in range(ns):
+        bsamps[el] = m.sample()
+    bsamps = np.array(bsamps)
+    c = ChainConsumer()
+    #Adding our samples to the chain, returning the chain
+    c.add_chain(sbi_data,parameters=["$x_1$", "$x_2$"],name='SBI')
+    c.add_chain(bsamps,parameters=["$x_1$", "$x_2$"],name='Bayesian')
+    return c
+
+#####################################################################
+#The following function makes chain consumer plots                  #
+#####################################################################
+
+### Dimensions of plot, list of posteriors, Torch observation (must be shape [N,2]), 
+### sample size for sampling from posterior for pairplots.
+### Depending on the dimensions of plot (whether axs is 1D, 2D, or scalar), 
+### a slightly different plotting method is used.
+def plot_chains(pdims,posts,obsv,samps):
+    i = 0
+    #Clearing cc, just in case
+    cc = [] 
+    if pdims[0] > 1:
+        j = 0
+        t = 1
+        fig, axs = plt.subplots(pdims[0],pdims[1],figsize=(15,15),sharex='col',sharey='row')
+        for el in posts:
+            #Creating cc and plotting it onon a subplot axs
+            #For info on ChainConsumer, see: https://samreay.github.io/ChainConsumer/chain_api.html
+            #Using the plot_contour function because it integrates with Matplotlib better.
+            cc = pairplot_comp(el,obsv,samps)
+            cc.plotter.plot_contour(axs[i,j],'$x_1$','$x_2',chains=['SBI','Bayesian']);
+            #Formatting plots
+            axs[i,j].set_xlabel('X1')
+            axs[i,j].set_ylabel('X2')
+            axs[i,j].set_aspect('equal','box')
+            axs[i,j].plot(theta_true*np.ones(np.shape(np.linspace(-5,5))),np.linspace(-5,5),'black')
+            axs[i,j].plot(np.linspace(-5,5),theta_true*np.ones(np.shape(np.linspace(-5,5))),'black')
+            axs[i,j].set_title('Iteration ' + str(t) )
+
+            j+=1
+            t+=1
+            #Move to the next row when needed
+            if j%pdims[1] == 0:
+                j = 0
+                i+=1
+        return fig
+    if pdims[0] == 1 and pdims[1] > 1:
+        fig, axs = plt.subplots(pdims[0],pdims[1],figsize=(15,15),sharex='col', sharey='row')
+        for el in posts:
+            #Creating and plotting CC
+            cc = pairplot_comp(el,obsv,samps)
+            cc.plotter.plot_contour(axs[i],'$x_1$','$x_2$',chains=['SBI','Bayesian']);
+            #Formatting
+            axs[i].set_xlabel('X1')
+            axs[i].set_ylabel('X2')
+            axs[i].plot(theta_true*np.ones(np.shape(np.linspace(-5,5))),np.linspace(-5,5),'black')
+            axs[i].plot(np.linspace(-5,5),theta_true*np.ones(np.shape(np.linspace(-5,5))),'black')
+            axs[i].set_title('Iteration ' + str(i+1))
+            print('Plotted plot ' + str(i) + '!')
+            i+=1
+        return fig
+    if pdims[0] ==1 & pdims[1] ==1:
+        fig, axs = plt.subplots(pdims[0],pdims[1],figsize=(15,15),sharex='col', sharey='row')
+        for el in posts:
+            #Creating and plotting CC
+            cc = pairplot_comp(el,obsv,samps)
+            cc.plotter.plot_contour(axs,'$x_1$','$x_2$',chains=['SBI','Bayesian']);
+            #Formatting
+            axs.set_xlabel('X1')
+            axs.set_ylabel('X2')
+            axs.plot(theta_true*np.ones(np.shape(np.linspace(-5,5))),np.linspace(-5,5),'black')
+            axs.plot(np.linspace(-5,5),theta_true*np.ones(np.shape(np.linspace(-5,5))),'black')
+            axs.set_title('Iteration ' + str(i+1))
+            i+=1
+    #The extent of my error handling :^ )
+    else:
+        print('Wrong dimensions!')
+        return
