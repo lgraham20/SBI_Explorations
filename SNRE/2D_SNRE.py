8a996c910897203f0b5cd54427011c7e2e54d1ea
lgraham20 <95656453+lgraham20@users.noreply.github.com>
2022-05-20 15:07:34 -0500

    Better commenting on SNRE

--- SNRE/2D_SNRE.py
+++ SNRE/2D_SNRE.py
@@ -10,12 +10,27 @@ from sbi.inference import SNRE, prepare_for_sbi, simulate_for_sbi
 ### Bayesian analaysis and plotting the results.
 from SNRE_func import *
 from SNRE_var import *
-hard_mode = False #Determines whether data is random or a specifically hard 2D data set
-figID = '3'
-#Defining our Prior
+
+### If hard_mode is true, the mock observation will 
+### be one that base SNPE w/ MAF struggled to
+### identify as a Gaussian.
+hard_mode = False
+
+### The filepath determines where the resulting
+### pairplots will be generated and named.
+filepath = 'SNRE_Randoms/4SNREGaussian.png'
+
+### Defining our Prior
+### This default prior is a uniform distribution, with the 
+### same bounds on each parameter. 
 prior = utils.BoxUniform(low=low*torch.ones(num_dim), high=high*torch.ones(num_dim)) #Creates our parameter space
 
-#Creating a simulated data observation
+### Creating a simulated data observation
+### Normally, this will just draw a set of 
+### random pairs of points from a 2D Gaussian
+### distribution and takes their means. 
+### If Hard Mode, it copies the bad 
+### data set instead.
 if not hard_mode:
     observation = theta_true + sigma_true*torch.randn((n_obs,num_dim)) #Notice, the same as our simulator below!
     observed_mean = torch.mean(observation,0)
@@ -25,31 +40,57 @@ else:
     observed_mean = torch.mean(observation,0)
     print('Here is the observed mean: ' + str(observed_mean))
 
-#Simulation: Suppose that observed data will be from Gaussian with StD=1
+### This is the simulator that our SBI will use to generate simulated data 
+### and train the neural net. Much like our mock observation, it takes 
+### in a tensor of means, draws 10 random points from a 2D Gaussian 
+### centered on the means, and then averages them together for 
+### a summary statistic. 
 def simulator(theta):
     simobs = theta + sigma_true*torch.randn((n_obs,num_dim))
     sim_mean = torch.mean(simobs,0)
     return sim_mean
 
-# Now it's time to begin the SBI.
+###########################################
+###########################################
+## Now it's time to begin the SBI. ########
+###########################################
+###########################################
+
+### Checks that our prior and simulator will work with SBI.
 simulator, prior = prepare_for_sbi(simulator, prior)
-#dense_estimator = posterior_nn(model='maf', hidden_features=100, num_transforms=5)
+
+##########################################################################################
+###################### Density Estimator Info ############################################
+### If you'd like to modify the meta-parameters, change the dense_estimator            ###
+### variable and plug it in for density_estimator in the inference definition.         ###
+### Alternatively, the density_estimator accepts a few prebuilt configurations         ###
+### identified with strings.                                                           ###
+### Use Neural Spline Flow with 'nsf' and Masked Autoregressive Flow with 'maf'.       ###
+### 'mdn' and 'made' are also available. They have not been tested.                    ###
+### If you don't specify a density estimator, the default 'maf' will be chosen.        ###
+###                                                                                    ###
+### The posterior_nn() function may also be imported and used to design                ###
+### a custom probability density estimator.                                            ###
+### from sbi.utils.get_nn_models import posterior_nn                                   ###
+### dense_estimator = posterior_nn(model='nsf', hidden_features=100, num_transforms=10)###
+##########################################################################################
+##########################################################################################
+
+### Defines our inference object, which represents the neural network.
 inference = SNRE(prior=prior)
 
+### Creates a list to hold each posterior the neural net generates
+### and defines are first proposal for the distribution as the prior.
 posteriors = []
-#mcmcs = []
 proposal = prior
 
-#posterior1 = infer(simulator, prior, 'SNRE',num_workers=3,num_simulations=sim_count)
-#posteriors.append(posterior1)
-
-#potential1 = posterior_estimator_based_potential(posterior_estimator=posterior1,prior=prior,x_o=observed_mean)
-#mcmc = MCMCPosterior(potential_fn=potential1,proposal=posterior1)
-#mcmcs.append(mcmc1)
-
-
+### For an explanation on multiround inference, https://www.mackelab.org/sbi/tutorial/03_multiround_inference/
+### In short, this loop trains our neural network across the entire proposed parameter space ("proposal"),
+### then it is fed the observation which it uses to narrow the parameter space and focus in on the area
+### closest to our observation.
 for _ in range(n_runs):
-    #Runs simulations for our simulator, based on the prior + simulator
+    #Uses our simulator to generate a number (sim_count) of mock observations across our
+    #proposed parameter space (prior).
     theta, x = simulate_for_sbi(simulator, proposal, num_simulations= sim_count,num_workers=3) 
     
     #Adds the simulated data to the inference object (and trains it)
@@ -59,9 +100,11 @@ for _ in range(n_runs):
     posterior = inference.build_posterior(density_estimator)
     #Adds that posterior to the list of posteriors
     posteriors.append(posterior)
-    #Trains around the specific observation
+    #Narrows our parameter space to ones the posterior thinks are likely given
+    #our observation.
     proposal = posterior.set_default_x(observed_mean)
-    print('Finished run ' + str(_) + '!')
+    #Then, loop repeats with the narrowed parameter space.
+    print('Finished run ' + str(_ + 1) + '!')
 
-fig = plot_chains(pdims = dimens_plot,posts = posteriors,obsv = observation,samps = 5000)
-plt.savefig('SNRE_Randoms/'+ figID + 'SNREGaussian.png')  
+fig = plot_chains(pdims = dimens_plot,posts = posteriors,obsv = observation,samps = 10000)
+plt.savefig(filepath)  
